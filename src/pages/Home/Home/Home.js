fcca0123dd62eefa05b77c0f4864e91eb0eda7ec
ehasanrobin <ehasanrobin44@gmail.com>
2022-09-03 00:37:52 +0600

    add search

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -6,12 +6,19 @@ import "./Home.css";
 
 const Home = () => {
     const [services,setServices] = useState([]);
+    const [searchEvents,setsearchEvents] = useState([]);
 
     useEffect(()=> {
         fetch("https://fierce-brook-97765.herokuapp.com/events")
         .then(res => res.json())
         .then(data => setServices(data))
     },[])
+
+    const handleSearch = event => {
+      const text = event.target.value;
+    const match = services.filter(service => service.name.includes(text));
+     setsearchEvents(match);
+    }
     return (
         <section className='home-section'>
            <Container className='text-center'>
@@ -22,6 +29,7 @@ const Home = () => {
                     placeholder="Recipient's username"
                     aria-label="Recipient's username"
                     aria-describedby="basic-addon2"
+                    onChange={handleSearch}
                     />
                     <Button variant="primary" className='px-4' id="button-addon2">
                     Search
@@ -31,7 +39,7 @@ const Home = () => {
             <div className=''>
                 <Row xs={1} md={2} xl={4}>
                      {
-                services.map(service => <Service key={service._id} service={service}></Service>)
+                searchEvents.map(service => <Service key={service._id} service={service}></Service>)
             }
                 </Row>
             </div>
